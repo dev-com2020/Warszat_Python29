f5e56e0ec5b48dbbb2215886e5f0c06529c2fe46
devcom2020 <kwp@tlen.pl>
2022-11-29 15:50:29 +0100

    wtorek 15:50

--- klasa4.py
+++ klasa4.py
@@ -1,18 +1,30 @@
+# class UserAccount:
+#     def __init__(self, user, passwd):
+#         self._username = user
+#         self._password = passwd
+#
+#     def get_username(self):
+#         return self._username
+#
+#     def set_username(self, user):
+#         self._username = user
+#
+#     def get_password(self):
+#         return self._password
+#
+#     def set_username(self, passwd):
+#         self._password = passwd
+
+
 class UserAccount:
     def __init__(self, user, passwd):
         self._username = user
         self._password = passwd
 
-    def get_username(self):
-        return self._username
-
-    def set_username(self, user):
-        self._username = user
-
-    def get_password(self):
+    @property
+    def password(self):
         return self._password
 
-    def set_username(self, passwd):
+    @password.setter
+    def password(self, passwd):
         self._password = passwd
-
-
