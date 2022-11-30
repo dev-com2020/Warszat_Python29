5805cd82902fc8bf2bdd5deda2e61ae1e18676aa
devcom2020 <kwp@tlen.pl>
2022-11-30 14:19:51 +0100

    środa 14:19

--- posortowanie.py
+++ posortowanie.py
@@ -1,3 +1,7 @@
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {2, 3, 5, 7}
+
+
 def sort_priority(values, group):
     def helper(x):
         if x in group:
@@ -7,6 +11,21 @@ def sort_priority(values, group):
     values.sort(key=helper)
 
 
-numbers = [8, 3, 1, 2, 5, 4, 7, 6]
-group = {2, 3, 5, 7}
+def sort_priority2(numbers, group):
+    found = False
+
+    def helper(x):
+        if x in group:
+            found = True
+            return (0, x)
+        return (1, x)
+
+    numbers.sort(key=helper)
+    return found
+
+
+found = sort_priority2(numbers, group)
+print('Znaleziono:', found)
 print(numbers)
+
+# print(numbers)
