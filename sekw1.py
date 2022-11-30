254dd99c0bdbbae56a5a9c1bace1c2369d4c8307
devcom2020 <kwp@tlen.pl>
2022-11-30 09:57:05 +0100

    środa 09:57

--- sekw1.py
+++ sekw1.py
@@ -1,4 +1,14 @@
 a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+print(a[::2])
+print(a[::-2])
+print(a[2::2])
+print(a[-2::-2])
+print(a[-2:2:-2])
+print(a[2:2:-2])
+
+
+
+
 # print('Dwa środkowe:', a[3:5])
 # print('Wszystkie poza skrajnymi:', a[1:7])
 assert a[:5] == a[0:5]
@@ -8,8 +18,26 @@ assert a[5:] == a[5:(len(a))]
 # print(a[2:-1])
 # print(a[-3:-1])
 
-b = a[3:]
-print('Przed:    ', b)
-b[1] = 99
-print('Po:    ', b)
-print('Bez zmian:   ',a)
+# b = a[3:]
+# print('Przed:    ', b)
+# b[1] = 99
+# print('Po:    ', b)
+# print('Bez zmian:   ', a)
+#
+# c = ['czerwony', 'pomarańczowy', 'żółty', 'zielony', 'niebieski', 'fioletowy']
+#
+# odds = c[::2]
+# evens = c[1::2]
+# print(odds)
+# print(evens)
+#
+# x = 'tomek'
+# y = x[::-1]
+# print(y)
+#
+# w = '寿司'
+# w.encode('utf-8')
+# z = w[::-1]
+# # z1 = z.decode('utf-8')
+# print(z)
+
