6c323546b982108c1f9ebee47e974e7f2c420b6d
devcom2020 <kwp@tlen.pl>
2022-11-30 12:00:39 +0100

    środa 12:00

--- klasa_danych.py
+++ klasa_danych.py
@@ -1,4 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+
 # class Vector:
 #     def __init__(self, x, y):
 #         self.x = x
@@ -27,7 +29,6 @@ from dataclasses import dataclass
 #         return self.x == other.x and self.y == other.y
 
 
-
 @dataclass
 class Vector:
     x: int
@@ -48,8 +49,45 @@ class Vector:
         )
 
 
-print(Vector(2, 3))
-print(Vector(5, 3) + Vector(1, 2))
-print(Vector(5, 3) - Vector(1, 2))
-print(Vector(5, 3) == Vector(1, 2))
-print(Vector(1, 2) == Vector(1, 2))
+@dataclass(frozen=True)
+class FrozenVector:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        '''dodawanie dwóch wektorów za pomocą +'''
+        return Vector(
+            self.x + other.x,
+            self.y + other.y,
+        )
+
+    def __sub__(self, other):
+        '''odejmowanie dwóch wektorów za pomocą -'''
+        return Vector(
+            self.x - other.x,
+            self.y - other.y,
+        )
+
+
+@dataclass
+class DataClassWithDefaults:
+    immutable: str = field(default="Statyczna wartość domyślna")
+    mutable: list = field(default_factory=list)
+
+
+print(DataClassWithDefaults())
+print(DataClassWithDefaults("Jakieś niemodyfikowalne dane"))
+print(DataClassWithDefaults(None, ['tomek', 'krzysiek']))
+print(DataClassWithDefaults(mutable=['tomek', 'krzysiek'], immutable="Test"))
+
+# print(Vector(2, 3))
+# print(Vector(5, 3) + Vector(1, 2))
+# print(Vector(5, 3) - Vector(1, 2))
+# print(Vector(5, 3) == Vector(1, 2))
+# print(Vector(1, 2) == Vector(1, 2))
+# print()
+# print(FrozenVector(2, 3))
+# print(FrozenVector(5, 3) + FrozenVector(1, 2))
+# print(FrozenVector(5, 3) - FrozenVector(1, 2))
+# print(FrozenVector(5, 3) == FrozenVector(1, 2))
+# print(FrozenVector(1, 2) == FrozenVector(1, 2))
