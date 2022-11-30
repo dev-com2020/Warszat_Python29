5aca2f51082958def30b8d8a932923263e3dc300
devcom2020 <kwp@tlen.pl>
2022-11-30 11:50:14 +0100

    środa 11:50

--- klasa_danych.py
+++ klasa_danych.py
@@ -1,7 +1,37 @@
+from dataclasses import dataclass
+# class Vector:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def __add__(self, other):
+#         '''dodawanie dwóch wektorów za pomocą +'''
+#         return Vector(
+#             self.x + other.x,
+#             self.y + other.y,
+#         )
+#
+#     def __sub__(self, other):
+#         '''odejmowanie dwóch wektorów za pomocą -'''
+#         return Vector(
+#             self.x - other.x,
+#             self.y - other.y,
+#         )
+#
+#     def __repr__(self):
+#         '''zwracanie tekstowej reprezentacji wektora'''
+#         return f"<Wektor: x={self.x}, y={self.y}>"
+#
+#     def __eq__(self, other):
+#         '''sprawdzanie czy dwa wektory są równe'''
+#         return self.x == other.x and self.y == other.y
+
+
+
+@dataclass
 class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    x: int
+    y: int
 
     def __add__(self, other):
         '''dodawanie dwóch wektorów za pomocą +'''
@@ -17,10 +47,9 @@ class Vector:
             self.y - other.y,
         )
 
-    def __repr__(self):
-        '''zwracanie tekstowej reprezentacji wektora'''
-        return f"<Wektor: x={self.x}, y={self.y}>"
 
-    def __eq__(self, other):
-        '''sprawdzanie czy dwa wektory są równe'''
-        return self.x == other.x and self.y == other.y
+print(Vector(2, 3))
+print(Vector(5, 3) + Vector(1, 2))
+print(Vector(5, 3) - Vector(1, 2))
+print(Vector(5, 3) == Vector(1, 2))
+print(Vector(1, 2) == Vector(1, 2))
