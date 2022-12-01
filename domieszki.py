2e3b49272a569cd6eaa9831eb962d1afbd4d5820
devcom2020 <kwp@tlen.pl>
2022-12-01 12:08:03 +0100

    czwartek 12:07

---
+++ domieszki.py
@@ -0,0 +1,33 @@
+class ToDictMixin:
+    def to_dict(self):
+        return self._traverse_dict(self.__dict__)
+
+    def _traverse_dict(self, instance_dict):
+        output = {}
+        for k, v in instance_dict.items():
+            output[k] = self._traverse(k, v)
+        return output
+
+    def _traverse(self, key, value):
+        if isinstance(value, ToDictMixin):
+            return value.to_dict()
+        elif isinstance(value, dict):
+            return self._traverse_dict(value)
+        elif isinstance(value, list):
+            return [self._traverse_dict(value.__dict__)]
+        elif hasattr(value, '__dict__'):
+            return self._traverse_dict(value.__dict__)
+        else:
+            return value
+
+
+class BinaryTree(ToDictMixin):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+tree = BinaryTree(10, left=BinaryTree(7, right=BinaryTree(9)),
+                  right=BinaryTree(13, left=BinaryTree(11)))
+print(tree.to_dict())
