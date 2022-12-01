fd09ab95be33ef332932cc8886ab6dfcbef19125
devcom2020 <kwp@tlen.pl>
2022-12-01 10:55:03 +0100

    czwartek 10:54

--- class_metod.py
+++ class_metod.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 
 
 class InputData:
@@ -41,6 +42,7 @@ def generate_inputs(data_dir):
     for name in os.listdir(data_dir):
         yield PathInputData(os.path.join(data_dir, name))
 
+
 def create_workers(input_list):
     workers = []
     for input_data in input_list:
@@ -48,3 +50,17 @@ def create_workers(input_list):
     return workers
 
 
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+
+    first, *rest = workers
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+def mapreduce(data_dir):
+    inputs = generate_inputs(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
