a3b0da8611fbf32248353684a6ac1e4b21af7889
devcom2020 <kwp@tlen.pl>
2022-12-01 15:02:31 +0100

    czwartek 15:02

--- importowanie_pdf.py
+++ importowanie_pdf.py
@@ -11,7 +11,7 @@ file2 = open('report2.pdf', 'rb')
 pdf2 = PyPDF2.PdfFileReader(file2)
 output_pdf.appendPagesFromReader(pdf2)
 
-with open('rezultat.pdf','wb') as out_file:
+with open('rezultat.pdf', 'wb') as out_file:
     output_pdf.write(out_file)
 
 file1.close()
