f56d4fb15de72ab5d1fcbe8db5eaa2415b768cc7
devcom2020 <kwp@tlen.pl>
2022-12-01 09:34:49 +0100

    czwartek 09:34

--- klasy_sposob1.py
+++ klasy_sposob1.py
@@ -8,24 +8,30 @@ class Gradebook:
     def add_student(self, name):
         self._grades[name] = defaultdict(list)
 
-    def report_grade(self, name, subject, grade):
+    def report_grade(self, name, subject, score, weight):
         by_subject = self._grades[name]
         grade_list = by_subject[subject]
-        grade_list.append(grade)
+        grade_list.append((score, weight))
 
     def average_grade(self, name):
         by_subject = self._grades[name]
-        total, count = 0, 0
-        for grades in by_subject.values():
-            total += sum(grades)
-            count += len(grades)
-        return total / count
+
+        score_sum, score_count = 0, 0
+        for subject, scores in by_subject.items():
+            subject_avg, total_weight = 0, 0
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+
+        score_sum += subject_avg / total_weight
+        score_count += 1
+        return score_sum / score_count
 
 
 book = Gradebook()
 book.add_student('Tomasz Kaniecki')
-book.report_grade('Tomasz Kaniecki', 'informatyka', 90)
-book.report_grade('Tomasz Kaniecki', 'WF', 95)
-book.report_grade('Tomasz Kaniecki', 'angielski', 75)
+book.report_grade('Tomasz Kaniecki', 'informatyka', 90, 0.05)
+book.report_grade('Tomasz Kaniecki', 'WF', 95, 0.15)
+book.report_grade('Tomasz Kaniecki', 'angielski', 75, 0.60)
 
 print(book.average_grade('Tomasz Kaniecki'))
