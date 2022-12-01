7d2b3bf43fa427eb15d0e43c1c17896b096a5d54
devcom2020 <kwp@tlen.pl>
2022-12-01 09:17:01 +0100

    czwartek 09:16

---
+++ klasy_sposob1.py
@@ -0,0 +1,13 @@
+class Gradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = []
+
+    def report_grade(self, name, score):
+        self._grades[name].append(score)
+
+    def average_grade(self, name):
+        grades = self._grades[name]
+        return sum(grades / len(grades))
