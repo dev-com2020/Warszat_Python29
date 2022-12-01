17d580606814ca3b9fef442c2e6a0fe32e5a3da7
devcom2020 <kwp@tlen.pl>
2022-12-01 09:27:34 +0100

    czwartek 09:27

--- klasy_sposob1.py
+++ klasy_sposob1.py
@@ -1,22 +1,31 @@
+from collections import defaultdict
+
+
 class Gradebook:
     def __init__(self):
         self._grades = {}
 
     def add_student(self, name):
-        self._grades[name] = []
+        self._grades[name] = defaultdict(list)
 
-    def report_grade(self, name, score):
-        self._grades[name].append(score)
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
 
     def average_grade(self, name):
-        grades = self._grades[name]
-        return sum(grades) / len(grades)
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
 
 
 book = Gradebook()
 book.add_student('Tomasz Kaniecki')
-book.report_grade('Tomasz Kaniecki', 90)
-book.report_grade('Tomasz Kaniecki', 95)
-book.report_grade('Tomasz Kaniecki', 75)
+book.report_grade('Tomasz Kaniecki', 'informatyka', 90)
+book.report_grade('Tomasz Kaniecki', 'WF', 95)
+book.report_grade('Tomasz Kaniecki', 'angielski', 75)
 
 print(book.average_grade('Tomasz Kaniecki'))
