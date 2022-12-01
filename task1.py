dce2946f1156d7388c09eabc055e5580e1f08d42
devcom2020 <kwp@tlen.pl>
2022-12-01 12:56:18 +0100

    czwartek 12:56

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import argparse
+import configparser
 
 
 def main(number, other_number):
@@ -10,5 +11,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n1', type=int, help="Liczba", default=1)
     parser.add_argument('-n2', type=int, help="Inna liczba", default=1)
+    parser.add_argument('--config', '-c', type=argparse.FileType('r'), help='Plik konfiguracyjny')
+
     args = parser.parse_args()
+    if args.config:
+        config = configparser.ConfigParser()
+        config.read_file(args.config)
+        args.n1 = int(config['ARGUMENTS']['n1'])
+        args.n2 = int(config['ARGUMENTS']['n2'])
+
     main(args.n1, args.n2)
