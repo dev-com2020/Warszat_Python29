d72d7fe1ecf4d5a085b29453c999ffde11cb81b8
devcom2020 <kwp@tlen.pl>
2022-12-02 14:01:51 +0100

    piątek 14:02

--- as2.py
+++ as2.py
@@ -1,22 +1,33 @@
 import asyncio
-import time
+# import time
+#
+#
+# def count():
+#     print("one")
+#     time.sleep(1)
+#     print("two")
+#
+#
+# def main():
+#     for _ in range(3):
+#         count()
+#
+#
+# if __name__ == '__main__':
+#
+#
+#     s = time.perf_counter()
+#     main()
+#     elapsed = time.perf_counter() - s
+#     print(f'{__file__} executed in {elapsed:0.2f} sec')
+def stuff():
+    print("*******")
 
+def py34_coro():
+    yield from stuff()
 
-def count():
-    print("one")
-    time.sleep(1)
-    print("two")
+async def py35_coro():
+    await stuff()
 
-
-def main():
-    for _ in range(3):
-        count()
-
-
-if __name__ == '__main__':
-
-
-    s = time.perf_counter()
-    main()
-    elapsed = time.perf_counter() - s
-    print(f'{__file__} executed in {elapsed:0.2f} sec')
+py34_coro()
+py35_coro()
