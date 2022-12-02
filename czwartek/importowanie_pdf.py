d54dabc85cb999409a62c9933b3523d85327651f
devcom2020 <kwp@tlen.pl>
2022-12-02 07:56:41 +0100

    piątek 8:00

--- czwartek/importowanie_pdf.py
+++ czwartek/importowanie_pdf.py
@@ -2,12 +2,12 @@ import PyPDF2
 
 output_pdf = PyPDF2.PdfWriter()
 
-file1 = open('raport.pdf', 'rb')
+file1 = open('../raport.pdf', 'rb')
 pdf1 = PyPDF2.PdfFileReader(file1)
 
 output_pdf.appendPagesFromReader(pdf1)
 
-file2 = open('report2.pdf', 'rb')
+file2 = open('../report2.pdf', 'rb')
 pdf2 = PyPDF2.PdfFileReader(file2)
 output_pdf.appendPagesFromReader(pdf2)
 
